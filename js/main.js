d3fffcf07af03aa8d1781878d85292041ff1c0f8
VietDucNg <vietducnguyen.fit@gmail.com>
2025-09-30 11:52:34 +0200

    build: add eventListener to change grid backgroundColor on click

--- js/main.js
+++ js/main.js
@@ -6,7 +6,16 @@ for (let i=1;i<=numGrid*numGrid;i++) {
     let grid = document.createElement('div');
     let gridSize = sketchBoard.offsetWidth/numGrid;
     console.log(gridSize);
-    grid.textContent = '.'
-    grid.style.cssText = `width:${gridSize}px;height:${gridSize}px`
+    grid.textContent = '.';
+    grid.style.cssText = `width:${gridSize}px;height:${gridSize}px`;
+    grid.classList.add('grid');
     sketchBoard.appendChild(grid);
 }
+
+function changeBgColor() {
+    this.style.backgroundColor = 'black';
+}
+
+const grids = document.querySelectorAll('.grid');
+grids.forEach(grid => grid.addEventListener('click',changeBgColor));
+
