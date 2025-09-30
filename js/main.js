8d93c9b1bebc0231f34b2befa8225007c5f6bf81
VietDucNg <vietducnguyen.fit@gmail.com>
2025-09-30 14:10:46 +0200

    feat: changeBgColor works on mouseover not only click

--- js/main.js
+++ js/main.js
@@ -6,16 +6,26 @@ for (let i=1;i<=numGrid*numGrid;i++) {
     let grid = document.createElement('div');
     let gridSize = sketchBoard.offsetWidth/numGrid;
     console.log(gridSize);
-    grid.textContent = '.';
     grid.style.cssText = `width:${gridSize}px;height:${gridSize}px`;
     grid.classList.add('grid');
     sketchBoard.appendChild(grid);
 }
 
+const grids = document.querySelectorAll('.grid');
+let isDrawing = false;
+
 function changeBgColor() {
+    isDrawing = true;
     this.style.backgroundColor = 'black';
 }
 
-const grids = document.querySelectorAll('.grid');
-grids.forEach(grid => grid.addEventListener('click',changeBgColor));
+grids.forEach(grid => {
+  grid.addEventListener('mousedown', changeBgColor);
+  grid.addEventListener('mouseover', function() {
+    if (isDrawing) this.style.backgroundColor = 'black';
+  });
+});
 
+document.addEventListener('mouseup', function() {
+  isDrawing = false;
+});
