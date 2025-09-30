f758246237a814e9bf6d2251488f8f9169c9a9c8
VietDucNg <vietducnguyen.fit@gmail.com>
2025-09-30 11:33:11 +0200

    build: add code to draw 16x16 grid

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const sketchBoard = document.querySelector('.sketchBoard');
+
+let numGrid = 16;
+
+for (let i=1;i<=numGrid*numGrid;i++) {
+    let grid = document.createElement('div');
+    let gridSize = sketchBoard.offsetWidth/numGrid;
+    console.log(gridSize);
+    grid.textContent = '.'
+    grid.style.cssText = `width:${gridSize}px;height:${gridSize}px`
+    sketchBoard.appendChild(grid);
+}
