f48c4a87b64c7122429e740cf662ca43afc38b4e
VietDucNg <vietducnguyen.fit@gmail.com>
2025-10-02 13:55:48 +0200

    feat: toggle active class for function buttons

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 const DEFAULT_NUM_GRID = 16;
+const DEFAULT_COLOR = '#00aafb';
 
 const sketchBoard = document.querySelector('.sketchBoard');
 const slider = document.querySelector('.slider');
 const pixelInfo = document.querySelector('.pixelInfo');
+const singleColorBtn = document.querySelector('.singleColorBtn');
+const colorChoiceBtn = document.querySelector('.colorChoiceBtn');
+const functionBtns = document.querySelectorAll('.functions-left button:not(.clearBtn');
+
 
 let numGrid = DEFAULT_NUM_GRID;
+let color = DEFAULT_COLOR;
 
 function drawGrid(numGrid) {
     let gridSize = sketchBoard.offsetWidth/numGrid;
-    console.log(sketchBoard.offsetWidth)
-    console.log(gridSize)
     for (let i=1;i<=(numGrid*numGrid);i++) {
         let grid = document.createElement('div');
         grid.style.cssText = `width:${gridSize}px;height:${gridSize}px`;
@@ -24,7 +28,9 @@ function getAllGrids() {
 }
 
 function changeBgColor() {
-    this.style.backgroundColor = 'black';
+    if (singleColorBtn.classList.contains('active')) {
+        this.style.backgroundColor = color;
+    }
 }
 
 function mousedownOnGrid() {
@@ -64,6 +70,27 @@ function applyChangNumGridListener() {
         applyDrawListener();
 })
 }
+
+// ////////////
+function clearActiveClass() {
+    functionBtns.forEach(btn => btn.classList.remove('active'));
+}
+
+function toggleActiveClass(btn) {
+    btn.classList.toggle('active');
+}
+
+function updateColor() {
+    color = colorChoiceBtn.value;
+}
+
+functionBtns.forEach(btn => btn.addEventListener('click', function () {
+    clearActiveClass();
+    toggleActiveClass(btn);
+}));
+
+colorChoiceBtn.addEventListener('input', updateColor);
+
 
 window.addEventListener('load', function() {
   drawGrid(numGrid);
